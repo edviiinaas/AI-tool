bd58e09b385a57f1aa569e738c6e258a509c293f
edviiinaas <84224167+edviiinaas@users.noreply.github.com>
2025-06-13 20:53:38 +0400

    chore: inject Supabase env vars in next.config.mjs and final fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,22 @@
+// next.config.mjs
+
+import 'dotenv/config'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Only ignore ESLint during development
     ignoreDuringBuilds: process.env.NODE_ENV === "development",
   },
   typescript: {
-    // Only ignore TypeScript errors during development
     ignoreBuildErrors: process.env.NODE_ENV === "development",
   },
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }
 }
 
 export default nextConfig
